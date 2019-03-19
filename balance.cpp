a29d2619cf23429e99379f526b506abdfed24564
Tina Huynh <tinahuynh9898@gmail.com>
2019-03-19 11:20:32 -0700

    update finished project

--- balance.cpp
+++ balance.cpp
@@ -5,52 +5,33 @@
 #include <cstring>
 using namespace std;
 
-struct PERSON {
-    char  Name[20];  
-    float Balance;
-	PERSON * next;
+struct PERSON
+{
+	char Name[20];
+	float Balance;
 };
 
 int linesOfData();
+PERSON * readData(int & N);
 string FindRichest(PERSON a[], int N);
 void Deposit(string CustName, PERSON a[], int N);
-void NewCopy(string filename, PERSON a[], int N);
+void NewCopy(PERSON a[], int N);
 void Display(PERSON a[], int N);
 
-	int main()
+int main()
 {
 	int N = linesOfData();
-	//PERSON a[N];
-	ifstream inFile;
-	string dataFile = "data.txt";
-	inFile.open("data.txt");
-
-	PERSON * owner;
-	string firstName, lastName;
-	int i = 0;
-	while (!inFile.eof()) {
-		inFile >> firstName >> lastName;
-		inFile >> owner->Balance;
-		string fullName = firstName + " " + lastName;
-		strcpy(owner->Name, fullName.c_str());	// (string).c_str() turns a string into a character string
-
-		 cout << owner->Name << "\t";
-		cout << owner->Balance << endl;
-
-		// this stores the information from the text file into the array a
-		//a[i] = * owner;
-		owner->next = new PERSON;
-		i++;
-	}
-
-	// cout << "The customer with maximum balance is " << FindRichest(a, N) << endl;
+	PERSON * personPtr;
+	personPtr = readData(N);
 	
-	// string CustName;
-	// cout << "Enter your full name to deposit money: ";
-	// getline(cin, CustName);
-	// Deposit(CustName, a, N);
-	// NewCopy(dataFile, a, N);
-	// Display(a, N);
+	cout << "The customer with maximum balance is " << FindRichest(personPtr, N) << endl;
+
+	string CustName;
+	cout << "Enter your full name to deposit money: ";
+	getline(cin, CustName);
+	Deposit(CustName, personPtr, N);
+	NewCopy(personPtr, N);
+	Display(personPtr, N);
 }
 
 int linesOfData()
@@ -60,22 +41,50 @@ int linesOfData()
 	int counter = 0;
 	string temp;
 
-	while (!inFile.eof()) {
+	while (!inFile.eof())
+	{
 		getline(inFile, temp);
 		counter++;
 	}
+	inFile.close();
 	return counter;
+}
+
+PERSON * readData(int &N)
+{
+	PERSON * personPtr;
+	personPtr = new PERSON[N];
+
+	ifstream inFile;
+	string dataFile = "data.txt";
+	inFile.open("data.txt");
+
+	//PERSON owner;
+	string firstName, lastName;
+	int i = 0;
+	while (!inFile.eof())
+	{
+		inFile >> firstName >> lastName;
+		inFile >> personPtr[i].Balance;
+		string fullName = firstName + " " + lastName;
+		strcpy(personPtr[i].Name, fullName.c_str()); // (string).c_str() turns a string into a character string
+		i++;
+	}
+
+	return personPtr;
 }
 
 string FindRichest(PERSON a[], int N)
 {
 	// find max balance
-	// Output:  	
+	// Output:
 	// The customer with maximum balance is Kimberly Macias
 	int richest = 0;
 	string name;
-	for (int i = 0; i < N; i++) {
-		if (a[i].Balance > richest) {
+	for (int i = 0; i < N; i++)
+	{
+		if (a[i].Balance > richest)
+		{
 			richest = a[i].Balance;
 			name = a[i].Name;
 		}
@@ -95,21 +104,24 @@ void Deposit(string CustName, PERSON a[], int N)
 	cout << CustName << ", how much would you like to deposit?  ";
 	cin >> deposit;
 
-	for (int i = 0; i < N; i++) {
-		if (strcmp(a[i].Name, CustName.c_str()) == 0) {
+	for (int i = 0; i < N; i++)
+	{
+		if (strcmp(a[i].Name, CustName.c_str()) == 0)
+		{
 			a[i].Balance += deposit;
 			cout << "Now your new balance is " << a[i].Balance << endl;
 		}
 	}
 }
 
-void NewCopy(string filename, PERSON a[], int N)
+void NewCopy(PERSON a[], int N)
 {
 	ofstream outFile;
-	outFile.open(filename);
+	outFile.open("data.txt");
 
 	//  Copy array a in the same file
-	for (int i = 0; i < N; i++) {
+	for (int i = 0; i < N; i++)
+	{
 		outFile << a[i].Name << " ";
 		outFile << a[i].Balance << endl;
 	}
@@ -120,7 +132,7 @@ void Display(PERSON a[], int N)
 	// 	Name           	  Balance
 	// ----------------------------------------------
 	// Maria  Brown  2100.90
-	// Jeffrey  Jackson  200.20 
+	// Jeffrey  Jackson  200.20
 	// Bernard  Smith  223.10
 	// Matthew  Davenport  1630.20
 	// Kimberly Macias  19100.90
@@ -128,7 +140,8 @@ void Display(PERSON a[], int N)
 
 	cout << "Name\t\t\tBalance" << endl;
 	cout << "-----------------------------" << endl;
-	for (int i = 0; i < N; i++) {
+	for (int i = 0; i < N; i++)
+	{
 		cout << left << setw(20) << a[i].Name << "\t";
 		cout << "$" << fixed << setprecision(2) << setw(10) << a[i].Balance << endl;
 	}
